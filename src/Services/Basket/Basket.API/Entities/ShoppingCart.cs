e2f19402f8f7787e72be2d32e373e829c53584a8
saber_amani_geeksltd <saber.amani@geeks.ltd.uk>
2021-07-02 00:26:16 +0430

    Basket API entities

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart() : this(string.Empty)
+        {
+
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+            ShoppingCartItems = new List<ShoppingCartItem>();
+        }
+
+
+        public string UserName { get; set; }
+
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        public decimal TotalPrice => ShoppingCartItems.Sum(x => x.Price * x.Quantity);
+    }
+}
