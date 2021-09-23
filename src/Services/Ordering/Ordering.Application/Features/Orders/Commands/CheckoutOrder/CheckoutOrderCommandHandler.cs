79f09d9014d22b15f55840a2535aa7a609285d31
saber_amani_geeksltd <saber.amani@geeks.ltd.uk>
2021-09-23 13:33:12 +0330

    Revert "Revert "CQRS implemention for Ordering""
    This reverts commit 789a5de4f2b3f28d690a2d4b62351a5c6b1dfe12.

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandHandler.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Models;
+using Ordering.Domain.Entities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    public class CheckoutOrderCommandHandler : IRequestHandler<CheckoutOrderCommand, int>
+    {
+        private readonly IMapper _mapper;
+        private readonly IEmailService _emailService;
+        private readonly IOrderRepository _repository;
+        private readonly ILogger<CheckoutOrderCommandHandler> _logger;
+
+        public CheckoutOrderCommandHandler(IMapper mapper, IOrderRepository repository, 
+            IEmailService emailService, ILogger<CheckoutOrderCommandHandler> logger)
+        {
+            _mapper = mapper;
+            _repository = repository;
+            _emailService = emailService;
+            _logger = logger;
+        }
+
+        public async Task<int> Handle(CheckoutOrderCommand request, CancellationToken cancellationToken)
+        {
+            var order = _mapper.Map<Order>(request);
+            var newOrder = await _repository.AddAsync(order);
+
+            await SendEmail(newOrder);
+
+            return newOrder.Id;
+        }
+
+        private async Task SendEmail(Order order)
+        {
+            var email = new Email { };
+
+            try
+            {
+                await _emailService.SendEmail(email);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Order {order.Id} failed due to an error with the mail service: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Ordering.Application.Features.Orders.Queries.GetOrdersList;
+using System.Collections.Generic;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQuery : IRequest<List<OrderViewModel>>
+    {
+        public GetOrdersListQuery(string userName)
+        {
+            UserName = userName;
+        }
+
+        public string UserName { get; set; }
+    }
+}
